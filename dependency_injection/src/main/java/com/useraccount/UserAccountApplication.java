76cb56b9ca0124af84cb7a6960276834753b2878
Jim Jerald Burton <jim.jerald.burton@mail.com>
2020-12-22 15:34:03 +0100

    Added calculation of elapsed time

--- dependency_injection/src/main/java/com/useraccount/UserAccountApplication.java
+++ dependency_injection/src/main/java/com/useraccount/UserAccountApplication.java
@@ -8,6 +8,12 @@ public class UserAccountApplication {
 		long startTime = System.currentTimeMillis();
 		Injector.startApplication(UserAccountApplication.class);
 		Injector.getService(UserAccountClientComponent.class).displayUserAccount();
-		long endime = System.currentTimeMillis();
+		long endTime = System.currentTimeMillis();
+		System.out.println("\tElapsed time: " + getFormattedDifferenceOfMillis(endTime, startTime) + " seconds\n");
+	}
+	
+	static String getFormattedDifferenceOfMillis(long value1, long value2) {
+		String valueFormatted = String.format("%04d", (value1 - value2));
+		return valueFormatted.substring(0, valueFormatted.length() - 3) + "," + valueFormatted.substring(valueFormatted.length() -3);
 	}
 }
