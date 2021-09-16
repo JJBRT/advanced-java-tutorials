ceeafa7ff128ed8719404025f89c78b97a9bd6c1
JJBRT <jim.jerald.burton@mail.com>
2021-09-16 12:30:07 +0200

    Improved example

--- reflection/src/main/java/com/github/jjbrt/reflection/ModulesExporter.java
+++ reflection/src/main/java/com/github/jjbrt/reflection/ModulesExporter.java
@@ -23,13 +23,19 @@ public class ModulesExporter {
     		Modules.exportAllToAll();
     	    Method method = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
     	    method.setAccessible(true);
-    	    ClassLoader classLoader = new URLClassLoader(new URL[] {});
+    	    ClassLoader classLoader = new URLClassLoader(new URL[] {}, null);
     	    method.invoke(
     	    	classLoader,
     	    	Resources.getClassPath(ModulesExporter.class).getURL()
     	    );
-    	    Class<?> fieldsHandlerClass = classLoader.loadClass("com.github.jjbrt.reflection.FieldsHandler");
-    	    System.out.println(fieldsHandlerClass.getName());
+    	    Class<?> fieldsHandlerClass = classLoader.loadClass(FieldsHandler.class.getName());
+    	    if (FieldsHandler.class != fieldsHandlerClass) {
+    	    	System.out.println(
+    	    		FieldsHandler.class.toString() + " and " + fieldsHandlerClass.toString() + " are loaded from the same bytecode but they are different instances:" +
+    	    		"\n\t" + FieldsHandler.class + " is loaded by " + FieldsHandler.class.getClassLoader() +
+    	    		"\n\t" + fieldsHandlerClass + " is loaded by " + fieldsHandlerClass.getClassLoader()
+    	    	);
+    	    }
     	} catch (Exception exc) {
     		exc.printStackTrace();
     	}
