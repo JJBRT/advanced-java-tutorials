ac2ee5e47c62bf5b32014c2220db038a4460d6f6
JJBRT <jim.jerald.burton@mail.com>
2021-09-17 16:18:20 +0200

    Added test

---
+++ reflection/src/main/java/com/github/jjbrt/reflection/AllModulesToAllModulesExporter.java
@@ -0,0 +1,39 @@
+package com.github.jjbrt.reflection;
+
+
+import static org.burningwave.core.assembler.StaticComponentContainer.Modules;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+
+public class AllModulesToAllModulesExporter {
+	
+	public static void main(String[] args) {
+        try {
+			execute();
+		} catch (Throwable exc) {
+			exc.printStackTrace();
+		}
+    }
+	
+	
+    public static void execute() {
+    	try {
+    		Modules.exportAllToAll();
+    		Class<?> bootClassLoaderClass = Class.forName("jdk.internal.loader.ClassLoaders$BootClassLoader");
+    		Constructor<? extends ClassLoader> constructor =
+    			ClassLoader.getPlatformClassLoader().getClass().getDeclaredConstructor(bootClassLoaderClass);
+    		constructor.setAccessible(true);
+    		Class<?> classLoadersClass = Class.forName("jdk.internal.loader.ClassLoaders");
+    		Method bootClassLoaderRetriever = classLoadersClass.getDeclaredMethod("bootLoader");
+    		bootClassLoaderRetriever.setAccessible(true);
+    		ClassLoader newBuiltinclassLoader = constructor.newInstance(bootClassLoaderRetriever.invoke(classLoadersClass));
+    		System.out.println(newBuiltinclassLoader + " instantiated");
+    	} catch (Exception exc) {
+    		exc.printStackTrace();
+    	}
+    }
+	
+	
+}
