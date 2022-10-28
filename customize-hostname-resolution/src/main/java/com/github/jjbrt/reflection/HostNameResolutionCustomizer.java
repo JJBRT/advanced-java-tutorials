e18bc5dc9db2b0172cbbf8f17e38cda2a29824f1
JJBRT <jim.jerald.burton@mail.com>
2022-10-28 22:20:05 +0200

    Added new tutorial

---
+++ customize-hostname-resolution/src/main/java/com/github/jjbrt/reflection/HostNameResolutionCustomizer.java
@@ -0,0 +1,80 @@
+package com.github.jjbrt.reflection;
+
+import static org.burningwave.core.assembler.StaticComponentContainer.ManagedLoggerRepository;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.burningwave.tools.dns.DefaultHostResolver;
+import org.burningwave.tools.dns.HostResolverService;
+import org.burningwave.tools.dns.MappedHostResolver;
+
+public class HostNameResolutionCustomizer {
+
+	public static void main(String[] args) {
+        try {
+			execute(loadConfiguration());
+		} catch (Throwable exc) {
+			exc.printStackTrace();
+		}
+    }
+
+	public static Map<String, String> loadConfiguration() {
+		Map<String, String> hostAliases = new LinkedHashMap<>();
+		hostAliases.put("my.hostname.one", "123.123.123.123");
+		hostAliases.put("my.hostname.two", "123.123.123.123");
+		hostAliases.put("my.hostname.three", "12.21.34.43");
+		hostAliases.put("my.hostname.four", "12.21.34.43");
+		return hostAliases;
+	}
+
+    public static void execute(Map<String, String> hostAliases) {
+
+    	//Installing the host resolvers
+		HostResolverService.INSTANCE.install(
+			new MappedHostResolver(hostAliases),
+			DefaultHostResolver.INSTANCE
+		);
+
+		printHostInfo("my.hostname.one");
+		printHostInfo("my.hostname.two");
+		printHostInfo("my.hostname.three");
+		printHostInfo("my.hostname.four");
+		printHostInfo("google.com");
+
+		//Restoring hostname resolving to default and cleaning the cache
+		HostResolverService.INSTANCE.reset();
+
+		//Now we have reset the hostnames in the map will not be resolved...
+		printHostInfo("my.hostname.one");
+		printHostInfo("my.hostname.two");
+		printHostInfo("my.hostname.three");
+		printHostInfo("my.hostname.four");
+		//... Except this
+		printHostInfo("google.com");
+
+		//Adding host aliases again
+		HostResolverService.INSTANCE.install(
+			new MappedHostResolver(hostAliases),
+			DefaultHostResolver.INSTANCE
+		);
+
+		//Retesting
+		printHostInfo("my.hostname.one");
+		printHostInfo("my.hostname.two");
+		printHostInfo("my.hostname.three");
+		printHostInfo("my.hostname.four");
+		printHostInfo("google.com");
+    }
+
+	public static void printHostInfo(String hostname) {
+		try {
+			InetAddress inetAddress = InetAddress.getByName(hostname);
+			ManagedLoggerRepository.logInfo(HostNameResolutionCustomizer.class::getName, "\n\n\tThe ip of hostname {} is {}\n\n", inetAddress.getHostName(), inetAddress.getHostAddress());
+		} catch (UnknownHostException exc) {
+			ManagedLoggerRepository.logError(HostNameResolutionCustomizer.class::getName,"\n\n\tUnable to resolve hostname {}\n\n", hostname);
+		}
+	}
+}
---
+++ customize-hostname-resolution/src/main/java/com/github/jjbrt/reflection/HostNameResolutionFromYAMLCustomizer.java
@@ -0,0 +1,90 @@
+package com.github.jjbrt.reflection;
+
+import static org.burningwave.core.assembler.StaticComponentContainer.Driver;
+import static org.burningwave.core.assembler.StaticComponentContainer.ManagedLoggerRepository;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+
+import org.burningwave.core.assembler.ComponentContainer;
+import org.burningwave.tools.dns.DefaultHostResolver;
+import org.burningwave.tools.dns.HostResolverService;
+import org.burningwave.tools.dns.MappedHostResolver;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+public class HostNameResolutionFromYAMLCustomizer {
+
+	public static void main(String[] args) {
+        try {
+			execute(() -> loadConfiguration("hosts.yml"));
+		} catch (Throwable exc) {
+			exc.printStackTrace();
+		}
+    }
+
+    @SuppressWarnings("unchecked")
+	public static List<Map<String, Object>> loadConfiguration(String fileName) {
+    	ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+    	try (InputStream inputStream = ComponentContainer.getInstance().getPathHelper().getResourceAsStream("hosts.yml")) {
+    		return (List<Map<String, Object>>)mapper.readValue(inputStream, Map.class).get("hostAliases");
+    	} catch (IOException exc) {
+			return Driver.throwException(exc);
+		}
+    }
+
+    public static void execute(Supplier<List<Map<String, Object>>> hostAliasesSupplier) {
+
+    	//Installing the host resolvers
+		HostResolverService.INSTANCE.install(
+			new MappedHostResolver(hostAliasesSupplier),
+			DefaultHostResolver.INSTANCE
+		);
+
+		printHostInfo("my.hostname.one");
+		printHostInfo("my.hostname.two");
+		printHostInfo("my.hostname.three");
+		printHostInfo("my.hostname.four");
+		printHostInfo("google.com");
+
+		//Restoring hostname resolving to default and cleaning the cache
+		HostResolverService.INSTANCE.reset();
+
+		//Now we have reset the hostnames in the map will not be resolved...
+		printHostInfo("my.hostname.one");
+		printHostInfo("my.hostname.two");
+		printHostInfo("my.hostname.three");
+		printHostInfo("my.hostname.four");
+		//... Except this
+		printHostInfo("google.com");
+
+		//Adding host aliases again
+		HostResolverService.INSTANCE.install(
+			new MappedHostResolver(hostAliasesSupplier),
+			DefaultHostResolver.INSTANCE
+		);
+
+		//Retesting
+		printHostInfo("my.hostname.one");
+		printHostInfo("my.hostname.two");
+		printHostInfo("my.hostname.three");
+		printHostInfo("my.hostname.four");
+		printHostInfo("google.com");
+    }
+
+	public static void printHostInfo(String hostname) {
+		try {
+			InetAddress inetAddress = InetAddress.getByName(hostname);
+			ManagedLoggerRepository.logInfo(HostNameResolutionCustomizer.class::getName, "\n\n\tThe ip of hostname {} is {}\n\n", inetAddress.getHostName(), inetAddress.getHostAddress());
+		} catch (UnknownHostException exc) {
+			ManagedLoggerRepository.logError(HostNameResolutionCustomizer.class::getName,"\n\n\tUnable to resolve hostname {}\n\n", hostname);
+		}
+	}
+
+}
